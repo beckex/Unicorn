d9852c9119fe9f6a82ed4c8b3fff087fc4e4faf7
Yiyi Wang <shd101wyy@sina.com>
2014-11-17 22:34:49 -0600

    Unicorn Project

---
+++ platforms/ios/www/js/signup.js
@@ -0,0 +1,35 @@
+// user sign up
+$("#user_signup_btn").click(function(){
+    // TODO:
+    // sanitize username and password
+    // check password and username not empty and valid
+    // ask user to enter password twice.
+    
+    console.log("Clicked sign up button");
+    var username = $("#signup_username").val();
+    var password = $("#signup_password").val();
+    $.ajax({
+            url: "http://planetwalley.com/postit_test/signup.php",
+            async: false,
+            type: "POST",
+            // 下面是发送的信息
+            data:{username: username,
+                  password: password,}
+        }).done(function(data){
+            if(data === "Failed"){
+                alert("Failed to register\nPlease try later");
+                return;
+            }
+            else if (data === "user_exists"){
+                alert("Failed to sign up\nusername " + username + " already existed");
+                return;
+            }
+            console.log(data);
+            window.localStorage["user_id"] = data; // data is the user_id
+            window.localStorage["status"] = "You dont have status yet"; // set user status
+            window.location.href = ("../app.html"); // begin to run app
+        }).fail(function(data){
+            alert("Failed");
+            console.log(data);
+        })}
+                           )
---
+++ www/js/change_status.js
@@ -0,0 +1,28 @@
+// user change status
+$("#change_status_btn").click(function(){
+    var user_id = window.localStorage["user_id"];
+    var status = $("#status").val();
+    if(status === "You dont have status yet") return;
+    $.ajax({
+            url: "http://planetwalley.com/postit_test/change_status.php",
+            async: false,
+            type: "POST",
+            // 下面是发送的信息
+            data:{user_id: user_id,
+                  status: status,}
+        }).done(function(data){
+            if(data === "Failed"){
+                alert("Failed to change status\nPlease try later");
+                return;
+            }
+            else if (data === "Success"){
+                window.localStorage["status"] = status;
+                alert("Status updated");
+            }
+            else{
+                alert("Failed to change status\nPlease try later");
+            }
+        }).fail(function(data){
+            alert("Failed");
+        })
+                              })
