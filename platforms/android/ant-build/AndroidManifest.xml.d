ead7f1a0efbf43a5f4df809069197cf4f5fd6161
Yiyi Wang <shd101wyy@sina.com>
2014-12-06 22:28:46 -0600

    restore code

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/ant-build/AndroidManifest.xml : \
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
+++
@@ -1,4 +0,0 @@
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/ant-build/CordovaApp.ap_ \
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/ant-build/classes.dex \
-/Applications/XAMPP/xamppfiles/htdocs/xampp/PostIt_test/Unicorn/platforms/android/CordovaLib/ant-build/classes.jar \
